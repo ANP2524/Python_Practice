79c7eafb3b7ae6a516b052d139f7a6164ecebfbc
ANP2524 <144516106+ANP2524@users.noreply.github.com>
2024-01-10 16:32:53 +0530

    Create Problem_09.py

---
+++ Problem_09.py
@@ -0,0 +1,9 @@
+# Create a tuple containing a sequence of numbers
+tuplex = 5, 10, 15, 20, 25
+# Print the contents of the 'tuplex' tuple
+print(tuplex)
+
+# Create a tuple with a single item (note the comma after the item)
+tuplex = 5,
+# Print the contents of the 'tuplex' tuple
+print(tuplex)
